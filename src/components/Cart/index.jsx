c92fa5011db9e268c1a0a197d138e1ab538491c2
Jahrelm <devjahrel@gmail.com>
2024-05-15 00:46:33 -0500

    implememting redux structure

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -10,7 +10,7 @@ export default function Cart ({ className, type }) {
       try {
         const response = await axios.get('http://localhost:8080/cart/list');
         setCartItems(response.data); 
-        setTotalCartCost(response.data.reduce((acc, item) => acc + parseFloat(item.price.replace('$', '')), 0)); 
+        setTotalCartCost(response.data.reduce((acc, item) => acc + parseFloat(item.cartCost.replace('$', '')), 0)); 
       } catch (error) {
         console.error('Error fetching cart items:', error);
       }
@@ -22,7 +22,7 @@ export default function Cart ({ className, type }) {
   return (
     <>
       <div
-        style={{ boxShadow: " 0px 15px 50px 0px rgba(0, 0, 0, 0.14)" }}
+        style={{ boxShadow: "0px 15px 50px 0px rgba(0, 0, 0, 0.14)" }}
         className={`w-[300px] bg-white border-t-[3px] ${type === 3 ? 'border-qh3-blue' : 'cart-wrappwer'}  ${
           className || ""
         }`}
@@ -31,47 +31,48 @@ export default function Cart ({ className, type }) {
           <div className="product-items h-[310px] overflow-y-scroll">
             <ul>
               {cartItems.map((item, index) => (
-                <li key={index} className="w-full h-full flex">
-                  <div className="flex space-x-[6px] justify-center items-center px-4 my-[20px]">
-                    <div className="w-[65px] h-full">
+                <li key={index} className="w-full h-full flex items-center justify-between border-b border-gray-200 px-4 py-3">
+                  <div className="flex items-center space-x-[10px]">
+                    <div className="w-[65px] h-[65px]">
                       <img
                         src={`${process.env.PUBLIC_URL}/assets/images/product-img-1.jpg`}
                         alt=""
                         className="w-full h-full object-contain"
                       />
                     </div>
-                    <div className="flex-1 h-full flex flex-col justify-center ">
-                      <p className="title mb-2 text-[13px] font-600 text-qblack leading-4 line-clamp-2 hover:text-blue-600">
+                    <div>
+                      <p className="text-[13px] font-600 text-qblack leading-4 line-clamp-2 hover:text-blue-600">
                         {item.title}
                       </p>
-                      <p className="price">
-                        <span className="offer-price text-qred font-600 text-[15px] ml-2">
-                          {item.price}
-                        </span>
+                      <p className="text-[15px] font-500 text-qred">
+                        {item.totalCost}
                       </p>
                     </div>
                   </div>
-                  <span className="mt-[20px] mr-[15px] inline-flex cursor-pointer">
-                    <svg
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      fill="none"
-                      className="inline fill-current text-[#AAAAAA] hover:text-qred"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M7.76 0.24C7.44 -0.08 6.96 -0.08 6.64 0.24L4 2.88L1.36 0.24C1.04 -0.08 0.56 -0.08 0.24 0.24C-0.08 0.56 -0.08 1.04 0.24 1.36L2.88 4L0.24 6.64C-0.08 6.96 -0.08 7.44 0.24 7.76C0.56 8.08 1.04 8.08 1.36 7.76L4 5.12L6.64 7.76C6.96 8.08 7.44 8.08 7.76 7.76C8.08 7.44 8.08 6.96 7.76 6.64L5.12 4L7.76 1.36C8.08 1.04 8.08 0.56 7.76 0.24Z" />
-                    </svg>
-                  </span>
+                  <div className="flex items-center">
+                    <span className="mr-3">{item.quantity}</span>
+                    <span className="inline-flex cursor-pointer">
+                      <svg
+                        width="8"
+                        height="8"
+                        viewBox="0 0 8 8"
+                        fill="none"
+                        className="inline fill-current text-[#AAAAAA] hover:text-qred"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M7.76 0.24C7.44 -0.08 6.96 -0.08 6.64 0.24L4 2.88L1.36 0.24C1.04 -0.08 0.56 -0.08 0.24 0.24C-0.08 0.56 -0.08 1.04 0.24 1.36L2.88 4L0.24 6.64C-0.08 6.96 -0.08 7.44 0.24 7.76C0.56 8.08 1.04 8.08 1.36 7.76L4 5.12L6.64 7.76C6.96 8.08 7.44 8.08 7.76 7.76C8.08 7.44 8.08 6.96 7.76 6.64L5.12 4L7.76 1.36C8.08 1.04 8.08 0.56 7.76 0.24Z" />
+                      </svg>
+                    </span>
+                  </div>
                 </li>
               ))}
             </ul>
           </div>
-          <div className="w-full px-4 mt-[20px] mb-[12px]">
+          <div className="w-full px-4 mt-5">
             <div className="h-[1px] bg-[#F0F1F3]"></div>
           </div>
-          <div className="product-actions px-4 mb-[30px]">
-            <div className="total-equation flex justify-between items-center mb-[28px]">
+          <div className="product-actions px-4 mb-5">
+            <div className="total-equation flex justify-between items-center mb-4">
               <span className="text-[15px] font-500 text-qblack">Subtotal</span>
               <span className="text-[15px] font-500 text-qred">${totalCartCost.toFixed(2)}</span>
             </div>
@@ -90,10 +91,10 @@ export default function Cart ({ className, type }) {
               </a>
             </div>
           </div>
-          <div className="w-full px-4 mt-[20px]">
+          <div className="w-full px-4 mt-5">
             <div className="h-[1px] bg-[#F0F1F3]"></div>
           </div>
-          <div className="flex justify-center py-[15px]">
+          <div className="flex justify-center py-3">
             <p className="text-[13px] font-500 text-qgray">
               Get Return within <span className="text-qblack">30 days</span>
             </p>
